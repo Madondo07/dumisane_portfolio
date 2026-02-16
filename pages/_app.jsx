c7b6a7bb31df62d477ddc56f649d319183c37f44
Madondo07 <dumisanemadondo926@gmail.com>
2026-02-16 09:04:56 +0200

    Preload build error fix

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -7,6 +7,8 @@ import '../src/components/skills.css';
 import '../src/components/skills-stats.css';
 import '../src/components/contact.css';
 import dynamic from 'next/dynamic';
+import Head from 'next/head';
+import heroFallback from '../src/assets/intro.png';
 const ThemeToggle = dynamic(() => import('../src/components/ThemeToggle'), { ssr: false });
 const Navbar = dynamic(() => import('../src/components/Navbar'), { ssr: false });
 import Footer from '../src/components/Footer';
@@ -15,13 +17,19 @@ export default function MyApp(props) {
   const Component = props.Component;
   const pageProps = props.pageProps;
   return (
-    <div className="min-h-screen app-root">
-      <ThemeToggle />
-      <Navbar />
-      <main>
-        <Component {...pageProps} />
-      </main>
-      <Footer />
-    </div>
+    <>
+      <Head>
+        {/* preload hero image to improve LCP discovery */}
+        <link rel="preload" as="image" href={heroFallback.src} />
+      </Head>
+      <div className="min-h-screen app-root">
+        <ThemeToggle />
+        <Navbar />
+        <main>
+          <Component {...pageProps} />
+        </main>
+        <Footer />
+      </div>
+    </>
   );
 }
